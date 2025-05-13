3873d3d6f510e0319e92ac7f5a023e0bcec20708
Samuel <samuel.thomas1109@outlook.com>
2025-05-13 19:32:55 -0300

    Refactor response handling and fix facility creation.

--- catalog/src/models/productModel.js
+++ catalog/src/models/productModel.js
@@ -1,6 +1,5 @@
 import {DataTypes} from 'sequelize';
 import database from '../config/database.js';
-import Facility from "./facilityModel.js";
 
 const Product = database.sequelize.define('Product', {
   name: {
